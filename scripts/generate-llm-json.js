e5b8283e30f25102b8e4979559af46b985f475ba
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2026-05-26 17:14:10 +0500

    Add script to generate absolute image URLs

--- scripts/generate-llm-json.js
+++ scripts/generate-llm-json.js
@@ -1,9 +1,11 @@
 const fs = require('node:fs')
 
+const SITE_URL = 'https://abdukahhar.uz'
 const data = JSON.parse(fs.readFileSync('lib/data/projects.json', 'utf8'))
 const output = data.map((p) => ({
   ...p,
   description: p.description.llm,
+  image: `${SITE_URL}/projects/${p.image}`,
 }))
 
 fs.writeFileSync('public/projects.json', JSON.stringify(output, null, 2))
